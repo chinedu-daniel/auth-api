c8acce2eba131dd326fce7734109c27b8d4485e3
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-06-17 10:45:20 +0800

    created the project structure and then setting up the basic program file

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
+
+let db;
+
+async function connectDB() {
+    await client.connect();
+
+    db = client.db(process.env.DB_NAME);
+
+    console.log("MongoDB Connected");
+}
+
+function getDB() {
+    return db;
+}
+
+module.exports = {
+    connectDB,
+    getDB
+};
